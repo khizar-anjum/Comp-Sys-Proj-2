8468db3400527d29afc2c0c74626b25701ab26b1
Christopher Rosenberger <chrisorosenberger@gmail.com>
2021-04-27 15:48:10 -0400

    Delete utils.c

--- src/utils.c
+++
@@ -1,6 +0,0 @@
-#include "utils.h"
-
-void simple_sighandler(int signum, siginfo_t *info, void *ptr)	{
-	printf("Hi I am process %d and I am handling signal %d\n", getpid(), signum);
-	printf("Signal originates from process %lu\n", (unsigned long)info->si_pid);
-}
